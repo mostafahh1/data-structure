4590d5f05077c81904ec7af3b9b11edffb2f3f15
mostafahh1 <mmoossttaaffaa6666663@gmail.com>
2026-04-13 22:31:42 -0700

    more fun

--- include/tree.h
+++ include/tree.h
@@ -2,8 +2,28 @@
 #include <iostream>
 using namespace std;
 
+
+struct node2
+{
+    node2* left;
+    int data;
+    node2* right;
+};
+
 class tree {
+private:
+    node2* root = nullptr;
+    void insert_(int value,node2* &pnode);
+    void inorder_(node2* node);
+    void preorder_(node2* node);
+    void postorder_(node2* node);
 public:
     tree();
     ~tree();
+    void insert(int value);
+    void inorder();
+    void preorder();
+    void postorder();
+    void deleteTree(node2* node);
+    node2* search(int value);
 };
--- src/tree.cpp
+++ src/tree.cpp
@@ -3,37 +3,83 @@
 tree::tree() {
 
 }
-tree::~tree() {}
+tree::~tree() {
+    deleteTree(root);
+}
 
 void tree::insert(int value){
-    if(root == nullptr){
-        root = new node2;
-        root->data = value;
-        root->left = nullptr;
-        root->right = nullptr;
-    }else{
-        node2* Root = root; 
-        insert_(value ,Root);
-    }
+    insert_(value,root);
 }
 
 
 void tree::insert_(int value,node2* &pnode){
 
-    if(pnode->data > value){
-        pnode = pnode->left;
-    }else{
-         pnode = pnode->right;     
-    }
-
     if(pnode == nullptr){
+
         node2* node = new node2;
         node->data = value;
         node->left = nullptr;
         node->right = nullptr;
         pnode = node;
+        return;
+    }
+
+
+
+    if(pnode->data > value){
+        // pnode = pnode->left;
+        // insert_(value ,nodel);
+        insert_(value ,pnode->left);
     }else{
-        insert_(value ,pnode);
+        // pnode = pnode->right;
+        // insert_(value ,noder);
+        insert_(value ,pnode->right);
     }
 
 }
+
+void tree::inorder(){
+    inorder_(root);
+}
+
+void tree::inorder_(node2* node){
+    if (node !=nullptr){
+        inorder_(node->left);
+        cout << node->data << " ";
+        inorder_(node->right);
+    }
+}
+
+void tree::preorder(){
+        preorder_(root);
+}
+
+void tree::preorder_(node2* node){
+    if (node !=nullptr){
+        cout << node->data << " ";
+        preorder_(node->left);
+        preorder_(node->right);
+    }
+}
+
+void tree::postorder(){
+    postorder_(root);
+}
+
+void tree::postorder_(node2* node){
+    if (node !=nullptr){
+        postorder_(node->left);
+        postorder_(node->right);
+        cout << node->data << " ";
+    }
+}
+
+
+void tree::deleteTree(node2* node){
+    if (node !=nullptr){
+        deleteTree(node->left);
+        deleteTree(node->right);
+        delete node;
+    }
+}
+
